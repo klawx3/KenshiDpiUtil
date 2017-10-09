8c86a06d3656960a357ebb0812f57ee32a36a8a1
klawx3 <klawx3@gmail.com>
2017-10-09 16:37:21 -0300

    all

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # KenshiDpiUtil
 Bot for discord to give edpi and more
+Uso
+!kdpi <dpi> <ow_sens> (resolucion: opcional : all)
+Ejemplo:
+!kdpi 800 5 1920x1080
---
+++ index.js
@@ -0,0 +1,108 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const discord_token = 'MzY3MDAwMTEyMzUxNjc0Mzc4.DL1DHA.VMfYtgT7jiHCo-qB-7CNfCzbQhw';
+
+const resolutions = [
+    {"width": 1280, "height": 720},
+    {"width": 1440, "height": 900},
+    {"width": 1600, "height": 900},
+    {"width": 1680, "height": 1050},
+    {"width": 1920, "height": 1080},
+    {"width": 2560, "height": 1080},
+    {"width": 2560, "height": 1440},
+    {"width": 2560, "height": 1080},
+    {"width": 3840, "height": 2160}
+];
+let factors = {};
+resolutions.forEach( (resolution) =>
+  factors[resolution.width + "x" + resolution.height] = {
+    width: resolution.width,
+    height: resolution.height,
+    ratio: resolution.width/resolution.height
+  }
+);
+function calc(dpi,sensitivity,fov,callback){
+    const inchp360 = (360 * 10 / 3) / (dpi * sensitivity * 0.022);
+    const cmp360 = 2.54 * inchp360;
+    const rawdotpdeg = dpi * inchp360 / 360;
+    const dotpdeg = rawdotpdeg / 2;
+    let output = {
+        "inchp360" : inchp360,
+        "cmp360" : cmp360,
+        "rawdotpdeg" : rawdotpdeg,
+        "dotpdeg" : dotpdeg
+    };
+    let rr = [];
+    Object.keys(factors).forEach( (resolution) =>{
+        const factor = factors[resolution];
+        const pixeldeg = factor.ratio * factor.height / fov;
+        const error = (pixeldeg > dotpdeg) ? true : false;
+        const pixelskipped = pixeldeg - dotpdeg;
+        const res = {
+            "resolution" : resolution,
+            "factor": factor,
+            "error": error,
+            "pixelskipped" : pixelskipped
+        };
+        rr.push(res);
+    });
+    output.resolutions = rr;
+    callback(output);
+};
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    const mensaje = msg.content;
+    if(mensaje.startsWith("!kdpi")){
+        commands = mensaje.split(" ");
+        if(commands.length < 3){
+            let replyy = "Uso !kdpi <dpi> <ow_sens> (resolucion: opcional : all) \n" ;
+            replyy += "Ejemplo : !kdpi 800 5 1920x1080";
+            msg.reply(replyy);
+        }else{
+            const dpi = parseInt(commands[1]);
+            const sens = parseInt(commands[2]);
+            let show_details = 0;
+            let res;
+            if(typeof commands[3] !== 'undefined') { 
+                res = commands[3];
+                show_details = (res === 'all') ? 1 : 2; // 1 all - 2 specific
+            }
+            calc(dpi,sens,103,output => {
+                let output_text = Number((output.cmp360).toFixed(2)) + " cm/360"+ "\n";
+                if(show_details == 1 ){ // all res
+                    for(let i = 0 ; i < output.resolutions.length ; i++){ 
+                        let r = output.resolutions[i];
+                        output_text += "Resolución: " + r.resolution + " " ;
+                        output_text += (r.error ? "CON saltos píxel" : "SIN saltos píxel") + "\n";
+                    }
+                }else if(show_details == 2){ // especific
+                    for(let i = 0 ; i < output.resolutions.length ; i++){ 
+                        let r = output.resolutions[i];
+                        if(r.resolution == res ){
+                            output_text += "Resolución: " + r.resolution + " " ;
+                            output_text += (r.error ? "CON saltos píxel" : "SIN saltos píxel") + "\n";
+                            if(r.error){                                
+                                output_text += "Existen " + Number((r.pixelskipped).toFixed(1)) + " píxels perdidos por movimiento";
+                            }                            
+                            break;
+                        }                        
+                    }
+                }        
+                msg.reply(output_text);
+            });
+        }        
+    }
+});
+  
+client.login(discord_token);
+
+
+
+
+
+
+
